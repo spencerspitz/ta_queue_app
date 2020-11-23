0752bd20f74500105ae538c2e9ae14b24995d917
Ubuntu <ubuntu@ip-172-31-90-177.ec2.internal>
2020-11-23 19:57:32 +0000

    Stack validations added

--- app/controllers/stacks_controller.rb
+++ app/controllers/stacks_controller.rb
@@ -1,14 +1,16 @@
 class StacksController < ApplicationController
     def index
+        #sorts the stack objects by a given variable if there is non given user is the default
         order = params[:order] || "user"
         @stack = Stack.order(order)
     end
     
     def new
-        @stack = Stack.new
+        
     end
     
     def create
+        #creates a new stack variable, gives a flash notice if it was successfully posted or now
         @stack = Stack.new(stack_params)
         if @stack.save
             flash[:notice] = "New Question has been posted"
@@ -20,12 +22,15 @@ class StacksController < ApplicationController
     end
     
     def show
+        #when you click on the description of a question it brings you to a page where you can:
+        #1 view the question and 2 add a comment to answer the question being asked
         id = params[:id]
         @stack = Stack.find(id)
     end
     
     private
     def stack_params
+        #params that are required when asking a question
         params.require(:stack).permit(:user, :topic, :coscclass, :comment)
     end
 end
